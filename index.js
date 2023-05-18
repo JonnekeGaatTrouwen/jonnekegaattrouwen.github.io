d233bae462e26ae391e3ecd35221ee9ba0b1e75a
JonnekeGaatTrouwen <133437246+JonnekeGaatTrouwen@users.noreply.github.com>
2023-05-18 08:19:57 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function loadJSON() {
     console.log('test');
     fetch('./test.json').then((response) => {
-        console.log("hi there");
+        console.log("test");
         console.log(response);
+        console.log(response.body);
+        response.json().then((x) => {
+            console.log('here', x);
+        })
         document.getElementById('json').innerHTML = JSON.stringify(response);
     })
+
+    fetch('./test.json').then((response) => response.json())
+    .then((json) => console.log('what?', json));
 }
